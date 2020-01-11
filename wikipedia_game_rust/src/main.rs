8710d702583a125cbb6744a6b6159e8808dbf918
nathancohen4299 <ncohen4299@gmail.com>
2020-01-11 18:30:25 -0500

    Attempting to fix lifetime errors

--- wikipedia_game_rust/src/main.rs
+++ wikipedia_game_rust/src/main.rs
@@ -1,13 +1,76 @@
 use crate::page::Page;
+use std::cmp::min;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap};
 
 mod page;
 
 fn main() {
-    let base = "https://en.wikipedia.org";
+    let source = "/wiki/Make_Your_Wish";
+    let dest = "/wiki/Oh_Ji-eun";
+    if let Some(value) = calculate_shortest_path(source, dest) {
+        println!("Shortest Path of Cost {}", value);
+    } else {
+        println!("Error: Shortest Path Not Found");
+    }
+}
 
-    let url = "/wiki/Make_Your_Wish";
+#[derive(Copy, Clone, Eq, PartialEq)]
+struct State<'a> {
+    cost: u32,
+    id: &'a str,
+}
 
-    let source = Page::new(format!("{}{}", base, url).as_str());
+impl<'a> Ord for State<'a> {
+    fn cmp(&self, other: &State) -> Ordering {
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| self.id.cmp(&other.id))
+    }
+}
+impl<'a> PartialOrd for State<'a> {
+    fn partial_cmp(&self, other: &State) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
-    
+fn calculate_shortest_path(source: &'static str, destination: &'static str) -> Option<u32> {
+    let mut dist: HashMap<&str, u32> = HashMap::new();
+    let mut min_heap = BinaryHeap::new();
+    dist.insert(source, 0);
+    min_heap.push(State {
+        id: source,
+        cost: 0,
+    });
+
+    while let Some(State { id, cost }) = min_heap.pop() {
+        if id == destination {
+            return Some(cost);
+        }
+
+        if let Some(distance) = dist.get(id) {
+            if cost > *distance {
+                continue;
+            }g
+        }
+        let current_page: Page = Page::new(source);
+        for s in current_page.links {
+            let next = State {
+                id: s.clone().as_str(),
+                cost: cost + 1,
+            };
+            if dist.contains_key(s.as_str()) {
+                let next_distance = *dist.get(s.as_str()).unwrap();
+                if next.cost < next_distance {
+                    *dist.get_mut(next.id).unwrap() = next.cost.clone();
+                    min_heap.push(next);
+                }
+            } else {
+                dist.insert(next.id, next.cost.clone());
+                min_heap.push(next);
+            }
+        }
+    }
+    None
 }
--- wikipedia_game_rust/src/page.rs
+++ wikipedia_game_rust/src/page.rs
@@ -8,25 +8,26 @@ impl Handler for Collector {
     }
 }
 
-pub struct Page{
+static BASE: &'static str = "https://en.wikipedia.org";
+
+pub struct Page {
     pub path: String,
-    pub links: Vec<String>
+    pub links: Vec<String>,
 }
 
-impl Page{
-    pub fn new(path: &str)-> Page {
-        Page{ path: String::from(path), links: get_urls(path) }
+impl Page {
+    pub fn new(path: &str) -> Page {
+        Page {
+            path: String::from(path),
+            links: get_urls(path),
+        }
     }
 }
-
-
-
-
 
 fn get_urls(url: &str) -> Vec<String> {
     let mut easy = Easy2::new(Collector(Vec::new()));
     easy.get(true).unwrap();
-    easy.url(url).unwrap();
+    easy.url(format!("{}{}", BASE, url).as_str()).unwrap(); // Combine Base with path here
     easy.perform().unwrap();
 
     let contents = easy.get_ref();
@@ -38,15 +39,14 @@ fn get_urls(url: &str) -> Vec<String> {
 
     let mut urls: Vec<String> = Vec::new();
 
-
     for div in fragment.select(&div_selector) {
         let id = div.value().attr("id").unwrap_or("");
         println!("id = {}", id);
         if id == "mw-content-text" {
             for element in div.select(&a_selector) {
                 let path = element.value().attr("href").unwrap();
-                if path.chars().next().unwrap() != '#' && path.len() >= 6 &&
-                    &path[..6] == "/wiki/"{
+                if path.chars().next().unwrap() != '#' && path.len() >= 6 && &path[..6] == "/wiki/"
+                {
                     urls.push(format!("{}", path));
                 }
             }
